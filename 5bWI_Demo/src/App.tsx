8d2347a2e78be40e076ee48659cf84d783915b67
Immi01 <immanuel.ehe@gmx.at>
2025-01-14 10:43:06 +0100

    changed Card content to be modifiable

--- 5bWI_Demo/src/App.tsx
+++ 5bWI_Demo/src/App.tsx
@@ -2,14 +2,15 @@ import './App.css'
 import Card from './components/Card';
 
 function App() {
+  let filler:string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aut alias necessitatibus exercitationem at veritatis.";
   return (
     <div className='bg-drab-dark-brown h-screen'>
       <h1 className='font-bold p-10 text-4xl text-right text-white'>HTL Dornbirn</h1>
       <div className="grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 m-5 w-100% justify-around">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card image="https://picsum.photos/300/150" name='Günter' description={'Hier ist der Günter '+filler}/>
+        <Card image="https://picsum.photos/300/150" name='Hans' description={'Hier ist der Hans '+filler}/>
+        <Card image="https://picsum.photos/300/150" name='Harald' description={'Hier ist der Harald '+filler}/>
+        <Card image="https://picsum.photos/300/150" name='Jonas' description={'Hier ist der Jonas '+filler}/>
       </div>
     </div>
   )
--- 5bWI_Demo/src/components/Card.tsx
+++ 5bWI_Demo/src/components/Card.tsx
@@ -1,15 +1,12 @@
 //import React from 'react';
 
-type Props = {}
-
-export default function Card({}: Props) {
+export default function Card(props: { image: string; name: string; description: string }) {
     return (
         <div className="">
-            <img src="https://picsum.photos/300/150" alt="placeholder" className="rounded-t-lg w-full"/>
+            <img src={props.image} alt="placeholder" className="rounded-t-lg w-full"/>
             <div className="bg-white rounded-b-lg p-4">
-                <h1 className="font-bold">Lorem ipsum dolor sit amet.</h1>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem nihil mollitia nobis vero nam architecto cupiditate fugit,
-                doloremque est ullam adipisci dolores recusandae officiis. Dolorum ex veritatis est cum fugiat?
+                <h1 className="font-bold">{props.name}</h1>
+                {props.description}
             </div>
         </div>
     )
