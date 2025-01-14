91f1e5c8a764a72fdda9096976d87f70bfb90e8e
Immi01 <immanuel.ehe@gmx.at>
2025-01-14 16:32:49 +0100

    added directus Headless CMS

--- 5bWI_Demo/src/App.tsx
+++ 5bWI_Demo/src/App.tsx
@@ -1,16 +1,68 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Card from './components/Card';
 
 function App() {
-  let filler:string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aut alias necessitatibus exercitationem at veritatis.";
+
+  const [people, setPeople] = useState([]);
+
+  type Person = {
+    firstname: string;
+    lastname: string;
+    image:string;
+    date:Date;
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8055/items/people").then(
+      (response) => {
+        response.json().then((data: any) => {
+          data.data.map((person:any)=>{
+            if (person.date != null)
+              person.date = new Date(person.date);
+            else
+              person.date = new Date;
+          })
+          setPeople(data.data);
+        });
+      }
+    );
+  }, []);
+
+  /*const testPerson:Person[] = [
+    {
+      firstname: "Günter",
+      lastname: "Mathis",
+      image: "https://picsum.photos/300/150",
+      date: new Date('December 17, 1995 03:24:00')
+    },
+    {
+      firstname: "Hans",
+      lastname: "Mathis",
+      image: "https://picsum.photos/300/150",
+      date: new Date('December 17, 2000 03:24:00')
+    },
+    {
+      firstname: "Harald",
+      lastname: "Gächter",
+      image: "https://picsum.photos/300/150",
+      date: new Date('November 27, 1965 13:24:00')
+    },
+    {
+      firstname: "Jonas",
+      lastname: "Korne",
+      image: "https://picsum.photos/300/150",
+      date: new Date('December 17, 2005 22:24:40')
+    },
+  ];*/
+
   return (
     <div className='bg-drab-dark-brown h-screen'>
       <h1 className='font-bold p-10 text-4xl text-right text-white'>HTL Dornbirn</h1>
-      <div className="grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 m-5 w-100% justify-around">
-        <Card image="https://picsum.photos/300/150" name='Günter' description={'Hier ist der Günter '+filler}/>
-        <Card image="https://picsum.photos/300/150" name='Hans' description={'Hier ist der Hans '+filler}/>
-        <Card image="https://picsum.photos/300/150" name='Harald' description={'Hier ist der Harald '+filler}/>
-        <Card image="https://picsum.photos/300/150" name='Jonas' description={'Hier ist der Jonas '+filler}/>
+      <div className="bg-drab-dark-brown grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 p-5 w-100% justify-around">
+        {people.map((person:Person) => <Card {...person} />)}
+        {/*testPerson.map((person:Person) => <Card {...person}/>)*/}
+
       </div>
     </div>
   )
--- 5bWI_Demo/src/components/Card.tsx
+++ 5bWI_Demo/src/components/Card.tsx
@@ -1,12 +1,20 @@
 //import React from 'react';
 
-export default function Card(props: { image: string; name: string; description: string }) {
+type Person = {
+  firstname: string;
+  lastname: string;
+  image:string;
+  date:Date;
+}
+
+export default function Card(person: Person) {
     return (
         <div className="">
-            <img src={props.image} alt="placeholder" className="rounded-t-lg w-full"/>
+            <img src={"http://localhost:8055/assets/" + person.image + "?width=300&height=150"} alt="placeholder" className="rounded-t-lg w-full"/>
             <div className="bg-white rounded-b-lg p-4">
-                <h1 className="font-bold">{props.name}</h1>
-                {props.description}
+                <h1 className="font-bold">{person.firstname}</h1>
+                This is {person.firstname} {person.lastname} born on {person.date.getFullYear()} and his Card.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia architecto asperiores commodi ullam nostrum excepturi?
             </div>
         </div>
     )
