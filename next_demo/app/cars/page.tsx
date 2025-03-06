babe7b1852385a1f07d009f0737add203f1daa18
Immi01 <immanuel.ehe@gmx.at>
2025-03-06 12:14:13 +0100

    Video 3

--- next_demo/app/cars/page.tsx
+++ next_demo/app/cars/page.tsx
@@ -4,21 +4,58 @@ import { Car } from '@/lib/types/types';
 
 type Props = {};
 
-const car: Car = {
-  "name":"SuperCar",
+const cars: Car[] = [{
+  "name":"Supercar",
   "type":"BMW X3",
   "color":"green",
   "motor":{
     "serialNumber":1234,
     "hp":200
   }
-};
+},
+{
+  "name":"Shitbox",
+  "type":"BMW 3",
+  "color":"browne",
+  "motor":{
+    "serialNumber":4321,
+    "hp":80
+  }
+},
+{
+  "name":"Hypercar",
+  "type":"Königsegg Jesko",
+  "color":"grey",
+  "motor":{
+    "serialNumber":89234,
+    "hp":1200
+  }
+},
+{
+  "name":"Eco Car (bad)",
+  "type":"Audi A3",
+  "color":"white",
+  "motor":{
+    "serialNumber":30845,
+    "hp":50
+  }
+},
+{
+  "name":"Best Car",
+  "type":"miat",
+  "color":"red",
+  "motor":{
+    "serialNumber":42069,
+    "hp":9999
+  }
+}
+];
 
 export default function page({}: Props) {
   return (
     <div>
       <h1>Cars</h1>
-      <Card car={car}/>
+      {cars.map((car) => <Card car={car}/>)}
     </div>
   )
 }
